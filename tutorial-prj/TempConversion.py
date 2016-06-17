b2136e858b5073f5887cd22a251ddc3434839540
prajwolthapa <prajwolthapa@hotmail.com>
2016-06-16 22:22:38 -0700

    Local File

---
+++ tutorial-prj/TempConversion.py
@@ -0,0 +1,15 @@
+# Temperarture Conversion Program
+
+def far2cel(far):
+    fnht =far
+    celcius = (5/9)*(fnht-32)
+    return  celcius
+
+def calvin(x):
+    x =temp
+    kelvin =x+273.15
+    return x
+
+temp = far2cel(32)
+
+print(temp)
